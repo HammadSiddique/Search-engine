177c225815dd80feea9f22cd0b3c92b432ca29ea
HammadSiddique <hammad.siddique01@gmail.com>
2022-12-07 20:17:29 +0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'search/index'
+  resources 'article', only: [:index]
+  get "/search", to: "search#index"
+  post 'article/search', to: 'article#search'
+  root 'article#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
